46cdf2726a45ca28c76ed498b7b60673735287eb
kairat-kempirbaev <7647704+kairat-kempirbaev@users.noreply.github.com>
2023-06-09 22:13:09 -0700

    Graph diameter by DFS from the root.
    If new max found iterate over the next furthest from the root. Repeat the same way from the furthest until no updates.

--- graph/DiameterBinaryTree.py
+++ graph/DiameterBinaryTree.py
@@ -4,20 +4,42 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        furthest = 1
-        furthestNode = None
-        if root is None:
-            return 0
-        def transformTree(root,parent,depth):
-            if root is None:
-                return
-            root.parent = parent
-            root.adjNodes= [root.right,root.left,parent]
-            depth +=1
+        longest = 0
 
-            transformTree(root.left, root.left,depth)
-            transformTree(root.right,root.right,depth)
-        tranformTree(root.left, root,1)
-        tranformTree(root.right,root,1)
+        if root is None:
+            return longest 
+
+        def transformTree(node,parent):
+            if node is None:
+                return
+
+            node.adjNodes= [ i for i in [node.right,node.left, parent] if i is not None]
+            transformTree(node.left, node)
+            transformTree(node.right, node)
+
+        transformTree(root,None)
+
+        visited = set()
+        theLast = root
+        keepRunning = True
+        def DFS(node,depth):
+            nonlocal longest, visited, keepRunning, theLast
+            visited.add(node)
+            if depth > longest:
+                keepRunning = True
+                theLast = node
+                longest = depth
+
+            for i in [ _ for _ in node.adjNodes if _ not in visited]:
+                visited.add(i)
+                DFS(i, depth+1)
+        while keepRunning:
+            keepRunning = False
+            DFS(theLast, 0)
+            visited = set()
+
+        return longest 
+

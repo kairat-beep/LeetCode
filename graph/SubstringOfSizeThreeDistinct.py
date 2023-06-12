24ba07809928b656e402e7c2128c05f000ba7f60
kairat-kempirbaev <7647704+kairat-kempirbaev@users.noreply.github.com>
2023-06-11 22:19:37 -0700

    Use set for the general case

---
+++ graph/SubstringOfSizeThreeDistinct.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        count = 0
+        for i in range(0,len(s)-2):
+            if len(set(s[i:i+3]))==3:
+                count+=1
+
+        return count
